d9d8888fdc455f78f997d4b0a899f04140c12ca3
aaravtheoriginal <aarav.raina4@gmail.com>
2020-08-23 15:49:36 +0530

    chain

--- sketch.js
+++ sketch.js
@@ -35,6 +35,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6=new Log(230,180,80,PI/2)
+    chain=new Chain(bird.body,log6.body)
+    
 
 }
 
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
+    log6.display();
 }
